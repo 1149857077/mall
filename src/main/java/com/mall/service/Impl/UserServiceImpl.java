33ac079ad1e856a4f1ad914b0d3a25ad79bc04c1
WangDongDong <1149857077@qq.com>
2018-12-19 13:44:54 +0800

    用户登录

---
+++ src/main/java/com/mall/service/Impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.mall.service.Impl;
+
+import com.mall.common.ServerResponse;
+import com.mall.dao.UserMapper;
+import com.mall.pojo.User;
+import com.mall.service.IUserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ */
+@Service("iUserService")
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    @Override
+    public ServerResponse login(String username, String password) {
+        int resultCount = userMapper.checkUsername(username);
+        if(resultCount==0){
+            return ServerResponse.creatByErrorMessage("用户名不存在");
+        }
+
+        //TODO 密码登录MD5
+
+        User user=userMapper.selectLogin(username,password);
+        if(user==null){
+            return ServerResponse.creatByErrorMessage("密码错误");
+        }
+
+        user.setPassword(StringUtils.EMPTY);
+        return ServerResponse.creatBySuccess("登录成功",user);
+    }
+}
